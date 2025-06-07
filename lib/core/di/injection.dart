549d19ff77eace202b5fe455053ec90a4f39b1da
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2025-06-08 02:36:53 +0300

    feat: register repositories in injection

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
+import 'package:invoice_generator/features/new_invoice/bloc/new_client_bloc/new_client_bloc.dart';
+import 'package:invoice_generator/features/new_invoice/data/repositories/new_client_repositories/new_client_repository_impl.dart';
 import 'package:invoice_generator/features/your_items/bloc/your_items_bloc.dart';
-import 'package:invoice_generator/features/your_items/data/repositories/abstract_your_items_repository.dart';
 import 'package:invoice_generator/features/your_items/data/repositories/your_items_repository_impl.dart';
 
 final getIt = GetIt.I;
@@ -8,4 +9,6 @@ final getIt = GetIt.I;
 void setupDependencies() {
   getIt.registerSingleton<YourItemsRepositoryImpl>(YourItemsRepositoryImpl());
   getIt.registerSingleton<YourItemsBloc>(YourItemsBloc(getIt<YourItemsRepositoryImpl>()));
+  getIt.registerSingleton<NewClientRepositoryImpl>(NewClientRepositoryImpl());
+  getIt.registerSingleton<NewClientBloc>(NewClientBloc());
 }
